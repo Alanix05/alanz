6dc18caacaad233a2f5ff852c5c1227d8782452f
Alanix05 <128261949+Alanix05@users.noreply.github.com>
2023-12-02 19:40:05 +0100

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,17 +1,45 @@
-function toggleDarkMode() {
+function toggleDarkMode(event) {
+    event.preventDefault();
     var element = document.body;
     element.classList.toggle("dark-mode");
     var darkButton = document.getElementById("darkmode");
 
     if (element.classList.contains("dark-mode")) {
         darkButton.innerHTML = "&#127773;";
-        localStorage.setItem("darkMode", "enabled"); 
+        localStorage.setItem("darkMode", "enabled");
     } else {
         darkButton.innerHTML = "&#127770;";
-        localStorage.setItem("darkMode", "disabled"); 
+        localStorage.setItem("darkMode", "disabled");
     }
-}
 
-if (localStorage.getItem("darkMode") === "enabled") {
-    toggleDarkMode(); 
-}
+    if (localStorage.getItem("darkMode") === "enabled") {
+        toggleDarkMode();
+    }
+
+    if (typeof(Storage) !== "undefined") {
+        if (sessionStorage.clickcount) {
+            sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+        } else {
+            sessionStorage.clickcount = 1;
+        }
+
+        
+
+        if (sessionStorage.clickcount == 5) {
+            element.classList.add("gradient");
+            darkButton.innerHTML = "&#127773;";
+            localStorage.setItem("darkMode", "enabled");
+        }
+        if (sessionStorage.clickcount != 5) {
+            element.classList.remove("gradient");
+        }
+        }else {
+            document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        }
+    }
+
+
+       
+            if (localStorage.getItem("darkMode") === "enabled") {
+                toggleDarkMode(); 
+        }
